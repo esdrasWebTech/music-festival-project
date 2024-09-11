7a965fb317c163f0fe203a6cc63e420f4d18407e
Esdras Ceballos Zurita <esdras.ceballosz@outlook.com>
2024-09-11 01:55:39 -0600

    gulpfile: added task and series function to compile js code

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 //import dependencies
-import { src, dest, watch } from 'gulp';
+import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 //connecting Gulp with SASS
 const sass = gulpSass(dartSass);
+
+//compiling js
+export function js(done){
+
+    src('./src/js/app.js')
+        .pipe(dest('./build/js'));   
+
+    done();
+}
 
 // compiling sass
 export function css(done){
@@ -19,5 +28,9 @@ export function css(done){
 // adding a watch for css function
 export function dev(){
     watch('./src/scss/**/*.scss', css);
+    watch('./src/js/**/*.js', js);
 };
+
+//export series function from Gulp
+export default series(js, css, dev);
 
