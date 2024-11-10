b2f4de35553117254b7c4644c61b7881350781ba
Esdras Ceballos Zurita <esdras.ceballosz@outlook.com>
2024-11-09 20:43:25 -0600

    gallery images modal added

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,55 @@ function createGalerie(){
         const image = document.createElement('img');
         image.src = `../img/grande/${i}.jpg`;
         image.alt = `Imagen galeria ${i}`;
-        console.log(image);
+        image.onclick = function(){
+            showModal(i);
+        };
 
         gallery.appendChild(image);
     };
+};
+
+// generating modal
+function showModal(i){
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.onclick = modalRemove;
+
+    //generatig modal inner container
+    const modalInner = document.createElement('div');
+    modalInner.classList.add('modal-inner');
+
+    //generating image view
+    const image = document.createElement('img');
+    image.src = `../img/grande/${i}.jpg`;
+    image.alt = `Imagen galeria ${i}`;
+
+    //generating modal close btn
+    const modalCloseBtn = document.createElement('button');
+    modalCloseBtn.classList.add('modal-close-btn');
+    modalCloseBtn.textContent = 'X';
+    modalCloseBtn.onclick = modalRemove;
+
+    //add modal in HTML
+    const bodyPage = document.querySelector('body');
+    bodyPage.classList.add('overflow-hidden');
+    modalInner.appendChild(modalCloseBtn);
+    modalInner.appendChild(image);
+    modal.appendChild(modalInner);
+    bodyPage.appendChild(modal);
+};
+
+//Close modal 
+function modalRemove (){
+    
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+    
+    setTimeout(() => {
+        const bodyPage = document.querySelector('body');
+        bodyPage.classList.remove('overflow-hidden');
+
+        modal?.remove();
+    }, 300)
+
 };
